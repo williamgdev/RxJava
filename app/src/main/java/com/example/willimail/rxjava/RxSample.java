5c122df3ebabdc5d14c4ffc13cb26baeb15f1b52
AndroidStorm <williamg.dev@gmail.com>
2018-02-08 12:42:01 -0800

    Create a List and Connect after finish the insertion process

--- app/src/main/java/com/example/willimail/rxjava/RxSample.java
+++ app/src/main/java/com/example/willimail/rxjava/RxSample.java
@@ -1,10 +1,19 @@
 package com.example.willimail.rxjava;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.observables.ConnectableObservable;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by willimail on 2/8/18.
@@ -12,7 +21,44 @@ import rx.functions.Func1;
 
 public class RxSample {
     public static void main(String[] args) {
-        flatMap();
+        Executor executor =
+                new ThreadPoolExecutor(5, 5, 200, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(10));
+        List<Integer> values = new ArrayList<>();
+        ConnectableObservable observable = Observable
+                .just(values)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.from(executor))
+                .publish();
+        Action1<List<Integer>> subscriber = new Action1<List<Integer>>() {
+            @Override
+            public void call(List<Integer> integers) {
+                System.out.print("New values: ");
+                for (Integer i : integers) {
+                    System.out.print(i + " ");
+                }
+                System.out.println("");
+            }
+        };
+        observable.subscribe(subscriber);
+        values.add(0);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        values.add(1);
+        values.add(2);
+        values.add(1);
+        values.add(2);
+        values.add(1);
+        values.add(2);
+        values.add(1);
+        values.add(2);
+        values.add(1);
+        values.add(2);
+        values.add(1);
+        values.add(0);
+        observable.connect();
     }
 
     private static void flatMap() {
