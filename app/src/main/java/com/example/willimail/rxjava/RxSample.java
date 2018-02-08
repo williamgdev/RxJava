b340de8435980517c3d7b9afef34216fa4ef8d60
AndroidStorm <williamg.dev@gmail.com>
2018-02-08 11:59:40 -0800

    Print Pairs

--- app/src/main/java/com/example/willimail/rxjava/RxSample.java
+++ app/src/main/java/com/example/willimail/rxjava/RxSample.java
@@ -2,13 +2,34 @@ package com.example.willimail.rxjava;
 
 import rx.Observable;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by willimail on 2/8/18.
  */
 
-public class ConcatSample {
+public class RxSample {
     public static void main(String[] args) {
+        printPairs();
+    }
+
+    private static void printPairs() {
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        return integer % 2 == 0;
+                    }
+                })
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        System.out.println(integer);
+                    }
+                });
+    }
+
+    private static void concat() {
         Observable<Integer> obsA = Observable.just(1, 2, 3, 4);
         Observable<Integer> obsB = Observable.just(5, 6, 7, 8);
 
@@ -31,6 +52,5 @@ public class ConcatSample {
                 });
 
         System.out.println("---------------------------");
-
     }
 }
